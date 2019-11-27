caf78f92ff7955015bd9e46e582e0cb04c95cd3f
MedZayd <34100943+MedZayd@users.noreply.github.com>
2019-11-27 21:34:27 +0100

    Home page component UI

---
+++ src/component/home-page/HomePage.component.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { Container } from "@material-ui/core";
+
+function HomePage() {
+  return <Container maxWidth={false}>Home Page Component</Container>;
+}
+
+export default HomePage;
